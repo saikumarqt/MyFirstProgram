c45c4f0dc4d33abe64b6250344d2200bd593fa1d
kumar6646 <sai.penusoft@gmail.com>
2025-05-22 11:39:02 +0530

    Wrote About Comments and Data Types.

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -10,7 +10,62 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
+            //These are data type with int and storing value of 100 in a varibale.
+            /*   int b = 100;
+               float s = 200.02f;
+               char m = 'a'; ;
+
+               string name = "abc";
+
+            */
+
+            //these are comments these are not compiled by compiler. these are excluded by compiler.
+            //we have 2 types of comments
+            /*
+             * 1)Single line comments : whenever we want to display some message to the developer
+             * using single line comment.we use by (//) as a single line comment .
+             * if we want to write message by using single line then we go for single line 
+             * comments.
+             * 2)MultiLine Comments: If a programmer want to write comments but it should be more thann
+             * one line then we will go for multi line comments.
+             * 
+             * Multi line comments are /*............*/
+
+            /*
+             * Data Types: where data is stored in a varible.
+             * Data types are segerated as values and reference types.
+             * values types: are nothing by fixed types.
+             * Integeral Types:
+             * short:what is there size and there ranges.
+             * int 
+             * long
+             * Signed Types:Both postive and negative values 
+             * short
+             * int
+             * long
+             * Unsgined Types:Only positive
+             * ushort
+             * uint
+             * ulong
+             * Floating Types
+             * Character types
+             * Boolean Types
+             * 
+             * Reference types:these are refering to the address.
+             * String
+             * Object
+             * 
+             */
+            int x = 100;
+            string name = "ASASDasd";
+
+
+            //program to print welcome messages 
+            int a = 100;
+            Console.WriteLine(a);
             Console.WriteLine("Welcome to C#");
+            Console.WriteLine("HI");
+            Console.WriteLine("Please enter Username");
         }
     }
 }
